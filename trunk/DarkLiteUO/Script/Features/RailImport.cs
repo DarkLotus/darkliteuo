2de0bde86779ba55027652f5692d9621b8d9083f
KKLOLK <KKLOLK@bdb09639-3117-5c47-7a8c-103312fc0347>
2010-11-24 12:50:04 +0000

    List<Point> ImportRail(string filename) Reads an easyuo created rail as long as it uses SET %X set %y and returns the values as a list of points.

--- trunk/DarkLiteUO/Script/Features/RailImport.cs
+++ trunk/DarkLiteUO/Script/Features/RailImport.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading;
 using UOLite2;
 using System.Windows.Forms;
@@ -12,7 +13,20 @@ namespace DarkLiteUO
 
     public partial class _ScriptTools
     {
-
+        public List<Point> ImportRail(String filename)
+        {
+            List<Point> rail = new List<Point>();
+            string[] text = File.ReadAllLines(filename);
+            for(int i = 0; i <= text.Length - 1;i++)
+            {
+                string line = text[i];
+                if(line.ToUpper().Contains("SET %X"))
+                {
+                    rail.Add(new Point(Convert.ToInt32(line.Substring(6)) ,Convert.ToInt32(text[i+1].Substring(6))));
+                }
+            }
+            return rail;
+        }
     
     }
 }
