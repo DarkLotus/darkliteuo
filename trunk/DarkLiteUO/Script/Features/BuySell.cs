0244dd6f7f0939bb7377174ca1f341800bb892f5
KKLOLK <KKLOLK@bdb09639-3117-5c47-7a8c-103312fc0347>
2010-11-24 11:42:51 +0000

    Split scripttools into seprate files Added Buy support

---
+++ trunk/DarkLiteUO/Script/Features/BuySell.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using UOLite2;
+using System.Windows.Forms;
+using Ultima;
+using System.Drawing;
+namespace DarkLiteUO
+{
+
+    public partial class _ScriptTools
+    {
+        private Serial _vendorid;
+        private ushort[] _items;
+        public void Buy(Serial VendorID, ushort[] Itemtypes)
+        {
+            if (!VendorID.IsValid) { return; }
+            _vendorid = null;
+            _items = Itemtypes;
+            Client.onPacketReceive += new LiteClient.onPacketReceiveEventHandler(HandleBuyWindowOpen);
+            Speak(Client.Mobiles.get_Mobile(VendorID).Name + " Buy");
+        }
+        public void Buy(Serial VendorID, ushort Itemtype)
+        {
+            if (!VendorID.IsValid) { return; }
+            _vendorid = null;
+            _items = new ushort[] { Itemtype };
+            Client.onPacketReceive += new LiteClient.onPacketReceiveEventHandler(HandleBuyWindowOpen);
+            Speak(Client.Mobiles.get_Mobile(VendorID).Name + " Buy");
+        }
+        private void HandleBuyWindowOpen(ref LiteClient Client, ref byte[] bytes)
+        {
+            if (bytes[0] != 0x74) { return; }
+             UOLite2.SupportClasses.BufferHandler buff = new UOLite2.SupportClasses.BufferHandler(bytes);
+             buff.Position = 3;
+             _vendorid = buff.readuint();
+            List<Item> Items = new List<Item>();
+             if (_vendorid.IsValid)
+             {
+                 Mobile vendor = Client.Mobiles.get_Mobile(_vendorid);
+                 foreach (Item i in vendor.Contents.Items)
+                 {
+                     if(_items.Contains<ushort>(i.Type))
+                     {
+                         Items.Add(i);
+                     }
+                 }
+                 BuyItems(_vendorid, Items);
+             }
+             Client.onPacketReceive -= HandleBuyWindowOpen;
+        }
+
+
+        private void BuyItem(Serial VendorID, Item Item)
+        {
+            UOLite2.SupportClasses.BufferHandler buff = new UOLite2.SupportClasses.BufferHandler(14);
+            
+            buff.WriteByte(0x3b);
+            buff.writeushort(14);
+            buff.writeuint(VendorID.Value);
+            buff.WriteByte(0x02);
+            buff.WriteByte(0x1A);
+            buff.writeuint(Item.Serial.Value);
+            buff.writeushort(Item.Amount);
+            byte[] bf = buff.buffer;
+
+            
+        }
+        private void BuyItems(Serial VendorID, List<Item> Items)
+        {
+            
+            UOLite2.SupportClasses.BufferHandler buff = new UOLite2.SupportClasses.BufferHandler((7+(Items.Count * 7)));
+
+            buff.WriteByte(0x3b);
+            buff.writeushort((ushort)(7 + (Items.Count * 7)));
+            buff.writeuint(VendorID.Value);
+            buff.WriteByte(0x02);
+            foreach (Item i in Items)
+            {
+                buff.WriteByte(0x1A);
+                buff.writeuint(i.Serial.Value);
+                buff.writeushort(i.Amount);
+            }
+                byte[] bf = buff.buffer;
+
+
+        }
+ 
+    
+    }
+}
---
+++ trunk/DarkLiteUO/Script/Features/ScriptTools.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using UOLite2;
+using System.Windows.Forms;
+using Ultima;
+using System.Drawing;
+namespace DarkLiteUO
+{
+
+    public class ScriptTools
+    {
+        public LiteClient Client;
+        public myTabPage GUI;
+        public _ScriptTools Tools;
+        
+
+        public ScriptTools(ref LiteClient _client, myTabPage _gui)
+        {
+            this.Client = _client;
+            this.GUI = _gui;
+            this.Tools = new _ScriptTools(ref Client, GUI);
+        }
+
+        public void Pathfind(ushort p, ushort p_2, ushort p_3)
+        {
+            Tools.Pathfind(p, p_2, p_3);
+        }
+    }
+      public partial class _ScriptTools
+    {
+        //Boolean Debug = false;
+        LiteClient Client;
+        myTabPage GUI;
+ 
+
+        public _ScriptTools(ref LiteClient Client, myTabPage GUI)
+        {
+            this.Client = Client;
+            this.GUI = GUI;
+            
+        }
+
+
+
+
+        public void Speak(String text)
+        {
+            UOLite2.Enums.Common.Hues myhue = UOLite2.Enums.Common.Hues.Blue;
+            UOLite2.Enums.SpeechTypes myspeech = UOLite2.Enums.SpeechTypes.Regular;
+            UOLite2.Enums.Fonts myfont = UOLite2.Enums.Fonts.Default;
+            string mymsg = text;
+            Client.Speak(ref mymsg, ref myhue, ref myspeech, ref myfont);
+        }
+
+        public uint EUOToInt(String val)
+        //Code by BtbN
+        {
+            val = val.ToUpper(); // Important!
+
+            uint num = 0;
+
+            for (int p = val.Length - 1; p >= 0; p--)
+                num = num * 26 + (((uint)val[p]) - 65);
+
+            num = (num - 7) ^ 0x45;
+
+            return num;
+        }
+        public ushort EUOToUshort(String val)
+        //Code by BtbN
+        {
+            val = val.ToUpper(); // Important!
+
+            uint num = 0;
+
+            for (int p = val.Length - 1; p >= 0; p--)
+                num = num * 26 + (((uint)val[p]) - 65);
+
+            num = (num - 7) ^ 0x45;
+
+            return (ushort)num;
+        }
+        public String IntToEUO(int num)
+        //Code by BtbN
+        {
+            num = (num ^ 0x45) + 7;
+
+            String res = "";
+
+            do
+            {
+                res += (Char)(65 + (num % 26));
+                num /= 26;
+            } while (num >= 1);
+
+            return res;
+        }
+        public int Get2DDistance(ushort X1, ushort Y1, ushort X2, ushort Y2)
+        {
+            //Taken from UOLite2
+            //Whichever is greater is the distance.
+            int xdif = Convert.ToInt32(X1) - Convert.ToInt32(X2);
+            int ydif = Convert.ToInt32(Y1) - Convert.ToInt32(Y2);
+
+            if (xdif < 0)
+                xdif *= -1;
+            if (ydif < 0)
+                ydif *= -1;
+
+            //Return the largest difference.
+            if (ydif > xdif)
+            {
+                return ydif;
+            }
+            else
+            {
+                return xdif;
+            }
+        }
+        public void GumpMenuSelection(uint ID, uint GumpID, uint ButtonID)
+        {
+            // static size atm buttons only
+            byte[] packet = new byte[23];
+            packet[0] = 0xb1;
+            packet[1] = 0x00;
+            packet[2] = 0x17;
+            
+            AddtoArray(3, ref packet, uintToByteArray(ID));
+            AddtoArray(7, ref packet, uintToByteArray(GumpID));
+            AddtoArray(11, ref packet, uintToByteArray(ButtonID));
+            AddtoArray(15, ref packet, uintToByteArray(0));
+            AddtoArray(19, ref packet, uintToByteArray(0));
+            GUI.UpdateLog("Sending Gump reply" + GetString(packet));
+            Client.Send(ref packet);
+        }
+        public static byte[] GetBytes(string text)
+        {
+            return ASCIIEncoding.UTF8.GetBytes(text);
+        }
+        public static String GetString(byte[] text)
+        {
+            return ASCIIEncoding.UTF8.GetString(text);
+        }
+        private static void AddtoArray(int index, ref byte[] Array, byte[] DatatoAdd)
+        {
+            DatatoAdd.CopyTo(Array, index);
+            //DatatoAdd.CopyTo(Array, Array.Length + 1);
+            //int startindex = Array.Length + 1;
+            //foreach (byte i in DatatoAdd)
+            //{
+            //    Array[startindex + i] = DatatoAdd[i];
+            //}
+            //return;
+        }
+        private static byte[] uintToByteArray(uint value)
+        {
+            return new byte[] {
+                (byte)(value >> 24),
+                (byte)(value >> 16),
+                (byte)(value >> 8),
+                (byte)value};
+
+        }
+
+
+    }
+               
+    
+
+   
+}
